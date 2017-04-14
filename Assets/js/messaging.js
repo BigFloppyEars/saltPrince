49060c00be59f04b41ed020071e8f2937e40b798
BigFloppyEars <cnfloss93@gmail.com>
2017-04-14 11:08:31 -0400

    start of messaging app

---
+++ Assets/js/messaging.js
@@ -0,0 +1,23 @@
+//jshint esversion:6 
+
+"use strict";
+
+requirejs.config({
+		//By default load any module IDs from js/lib
+		baseUrl: 'js'
+	});
+
+	// Load all Modules.
+	requirejs(
+		[
+		
+		],	
+	// All Modules Loaded.
+	
+	
+// Start "root" or "main" module/ script.	
+function(){
+
+	$("li").html("list item");
+	
+});
--- Controllers/serverController.js
+++ Controllers/serverController.js
@@ -2,17 +2,23 @@ const fs = require("fs");
 
 module.exports = function(app) {
 	
+	// Get Request Handlers
 	app.get("/laststand", function(req, res){
 		res.render("laststand");
 	});
 	
-	app.post("/laststand", function(req, res){
-		res.send(JSON.stringify(req.body));
-		console.log(req.body.data);
+	app.get("/messaging", function(req, res){
+		res.render("messaging");
 	});
 	
 	app.get("/", function(req, res){
 		fs.createReadStream("404.html").pipe(res);
+	});
+	
+	// Post Rquest Handlers
+	app.post("/laststand", function(req, res){
+		res.send(JSON.stringify(req.body));
+		console.log(req.body.data);
 	});
 
 };
