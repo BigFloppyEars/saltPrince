d2160e271364eaa78de86037963aa9fc80468324
BigFloppyEars <cnfloss93@gmail.com>
2017-05-03 19:17:31 -0400

    gett ready to be hosted in the cloud

--- Controllers/databaseController.js
+++ Controllers/databaseController.js
@@ -1,19 +1,46 @@
+const nconf = require("nconf");
 const mongoose = require("mongoose");
 
 // Set up Schema
 const Schema = mongoose.Schema;
 
 module.exports = function(app) {
+  nconf.argv().env().file('keys.json');
 
+// Connect to a MongoDB server provisioned over at
+// MongoLab.  See the README for more info.
+
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
+
+// [START client]
+let uri = `mongodb://${user}:${pass}@${host}:${port}`;
+
+if (nconf.get('mongoDatabase')) {
+  uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+
+console.log(uri);
+
+mongodb.MongoClient.connect(uri, (err, db) => {
+  if (err) {
+    throw err;
+  }
+  console.log(db);
+});
 	// connect to mongo database
-	mongoose.connect("mongodb://localhost/saltPrince");
+//	mongoose.connect("mongodb://localhost/saltPrince");
 
-	let db = mongoose.connection;
+//	let db = mongoose.connection;
 
+/*
 	db.once('open', function() {
 		// we're connected!
 		console.log("Connected to message database.".blue.whiteBG);
 	}).on('error', console.error.bind(console, 'connection error:'));
+*/
 
 	const messageSchema = Schema({
 		user: String,
